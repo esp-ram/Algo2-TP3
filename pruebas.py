ac75c4fd9b14b1d1a4cd07766944862416a19b02
Ramiro <esperon.ramiro@gmail.com>
2019-12-08 22:08:40 -0300

    recorrido de n viajes

--- pruebas.py
+++ pruebas.py
@@ -21,12 +21,8 @@ p = prim(tiempos,"BAT")
 
 total = 0
 for key in p:
-    # print(key)
     for k1 in p[key]:
-        # print(k1)
-        # print(p[key][k1])
         total += p[key][k1]
-###
 
 
 
@@ -44,44 +40,54 @@ def recorrida(grafo, lista, inicio, longMaximo, longActual,resultado):
 
     if completo == True:
         print("cumple",lista,"long",longActual)
-        # print("respuesta")
         pap = lista.copy()
-        # print("pap:",pap)
         longMaximo.append(longActual)
         resultado.append(pap)
         return
 
     for vertice in ver_adyacentes(grafo,inicio):
         lista.append(vertice)
-        # print("agrego vertice",vertice,"adyacente de",inicio)
         if recorrida(grafo,lista,vertice,longMaximo,longActual+obtener_peso(grafo,inicio,vertice),resultado) != True:
             lista.pop()
             continue
         longMaximo = longActual+obtener_peso(grafo,inicio,vertice)
-        # lista.append(vertice)
-
-    return
-# r=0
 
 
 
+def N_Lugares(grafo, final, inicio, cantidad, lista, n,resultado):
+    if n == cantidad:
+        # print("n == cantidad y ultimo vertice",lista[len(lista)-1])
+        if lista[len(lista)-1] in ver_adyacentes(grafo,final):
+            # print("CAMINO COMPLETO!!!!!!!!!!!")
+            pap = lista.copy()
+            resultado.append(pap)
+            return lista
+        return False
+
+    for vertex in ver_adyacentes(grafo,inicio):
+        # print("ady {} para vertice {}".format(vertex,inicio))
+        if vertex in lista:
+            # print("vertice evisitado")
+            continue
+        lista.append(vertex)
+        if N_Lugares(grafo,final,vertex,cantidad,lista,n+1,resultado) != True:
+            lista.pop()
+            continue
+
+
+"""
 lista = []
 respuesta = []
 largos = []
 lista.append("BAT")
 largos.append(total*2)
 recorrida(tiempos,lista,"BAT",largos,0,respuesta)
-# print(respuesta)
-# print(largos)
-
 print("Mejor ruta: {}, Costo: {}".format(respuesta[len(respuesta)-1],largos[len(largos)-1]))
-
-"""
-for i in range(len(respuesta)):
-    print("ruta: {} , costo: {}".format(respuesta[i],largos[i+1]))
 """
 
-"""
-BAT -> LAN -> ASH -> BH6 -> NAR -> ATL -> JFK -> ATL -> SHE -> RIV -> WAC
-Costo: 2644
-"""
+
+l1 = []
+l1.append("BAT")
+r1 = []
+print(N_Lugares(tiempos,"BAT","BAT",5,l1,0,r1))
+print(r1)
