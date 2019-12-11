b4399c2fe98c9251adff78d265000de2ac55e6e1
Ramiro <esperon.ramiro@gmail.com>
2019-12-10 23:31:43 -0300

    pagerank

--- pruebas.py
+++ pruebas.py
@@ -1,29 +1,31 @@
 from grafos import *
 from util_grafos import *
 import csv
+import random
 
 
 
 tiempos = crear_grafo()
+copia = []
 
-with open('vuelos_inventados.csv', "r") as csvfile:
+with open('vuelos.csv', "r") as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
     for row in spamreader:
         agregar_vertice(tiempos,row[0])
         agregar_vertice(tiempos,row[1])
+        copia.append(row)
 
-with open('vuelos_inventados.csv', "r") as csvfile:
+with open('vuelos.csv', "r") as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
     for row in spamreader:
         agregar_arista(tiempos,row[0],row[1],int(row[2]))
 
-p = prim(tiempos,"BAT")
+# p = prim(tiempos,"SAN")
 
-total = 0
-for key in p:
-    for k1 in p[key]:
-        total += p[key][k1]
-
+# total = 0
+# for key in p:
+#     for k1 in p[key]:
+#         total += p[key][k1]
 
 
 
@@ -42,7 +44,7 @@ def recorrida(grafo, lista, inicio, longMaximo, longActual,resultado):
     # 164 es el minimo de costo minimo de vuelos_inventados
     # si la cantidad de vuelos que faltan multiplicado por el costo minimo igualmente se
     # excede del maximo se descarta
-    if ((i-1)*164 + longActual) >= longMaximo[len(longMaximo)-1]:
+    if ((i)*24 + longActual) >= longMaximo[len(longMaximo)-1]:
         return False
 
 
@@ -54,6 +56,7 @@ def recorrida(grafo, lista, inicio, longMaximo, longActual,resultado):
         return
 
     for vertice in ver_adyacentes(grafo,inicio):
+        print("inicio: {}, adyacente: {}".format(inicio,vertice))
         lista.append(vertice)
         if recorrida(grafo,lista,vertice,longMaximo,longActual+obtener_peso(grafo,inicio,vertice),resultado) != True:
             lista.pop()
@@ -73,9 +76,11 @@ def N_Lugares(grafo, final, inicio, cantidad, lista, n,resultado):
         return False
 
     for vertex in ver_adyacentes(grafo,inicio):
+        if len(resultado) != 0:
+            return
         # print("ady {} para vertice {}".format(vertex,inicio))
         if vertex in lista:
-            # print("vertice evisitado")
+            # print("vertice visitado")
             continue
         lista.append(vertex)
         if N_Lugares(grafo,final,vertex,cantidad,lista,n+1,resultado) != True:
@@ -84,19 +89,64 @@ def N_Lugares(grafo, final, inicio, cantidad, lista, n,resultado):
 
 
 
+def pagerank(grafo,diccionario):
+    tamano = len(ver_vertices(grafo))
+    for vertice in ver_vertices(grafo):
+        total = 0
+        for adyacente in ver_adyacentes(grafo,vertice):
+            total += (diccionario[adyacente] / (len(ver_adyacentes(grafo,adyacente))))
+        diccionario[vertice] = ((1-0.85)/tamano) + (0.85)*total
+    return diccionario
+
+
+
+"""
 lista = []
 respuesta = []
 largos = []
-lista.append("BAT")
-largos.append(total*2)
-recorrida(tiempos,lista,"BAT",largos,0,respuesta)
-print("Mejor ruta: {}, Costo: {}".format(respuesta[len(respuesta)-1],largos[len(largos)-1]))
-
+lista.append("SAN")
+print(total)
+# minimo teorico 8500
+# minimo mst 22250
+largos.append(8500)
+# recorrida(tiempos,lista,"SAN",largos,0,respuesta)
+# print("Mejor ruta: {}, Costo: {}".format(respuesta[len(respuesta)-1],largos[len(largos)-1]))
+"""
 
 """
 l1 = []
-l1.append("BAT")
+l1.append("SAN")
 r1 = []
-print(N_Lugares(tiempos,"BAT","BAT",5,l1,0,r1))
+N_Lugares(tiempos,"SAN","SAN",5,l1,0,r1)
 print(r1)
 """
+
+factor = 1/len(ver_vertices(tiempos))
+dicc = {}
+for vertice in ver_vertices(tiempos):
+    dicc[vertice] = factor
+
+# for i in range(100):
+#     pagerank(tiempos,dicc)
+lista_diccs = []
+lista_diccs.append(dicc)
+i=0
+lista_diccs.append(pagerank(tiempos,lista_diccs[i]))
+i+=1
+ban = True
+while(ban):
+    for vertice in ver_vertices(tiempos):
+        if abs(lista_diccs[i][vertice] - lista_diccs[i-1][vertice]) < 0.09:
+            ban = False
+            break
+    lista_diccs.append(pagerank(tiempos,lista_diccs[i]))
+    i += 1
+
+
+li = []
+for key in dicc:
+    li.append((key,dicc[key]))
+
+li.sort(key = operator.itemgetter(1))
+
+print(li)
