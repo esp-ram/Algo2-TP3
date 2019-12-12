ef076e6bb2c385accd5cb281fe5500bb73c2ab36
Ramiro <esperon.ramiro@gmail.com>
2019-12-12 02:00:34 -0300

    vacaciones y menu

--- flycombi.py
+++ flycombi.py
@@ -3,6 +3,9 @@ from util_grafos import *
 import csv
 import sys
 import os
+COMANDOS_DISPONIBLES = ["camino_mas","camino_escalas","pagerank","centralidad"]
+
+
 
 def listar_operaciones():
     print("camino_mas")
@@ -23,7 +26,7 @@ def camino_mas_ops(origen,destino,grafo,archivoAero):
     aeroOrigen = obtener_aeropuertos(origen,archivoAero)
     aeroDestino = obtener_aeropuertos(destino,archivoAero)
     resultados = []
-    # que pasa si no hay caminos?
+    # REVIEW: que pasa si no hay caminos?
     for salida in aeroOrigen:
         for llegada in aeroDestino:
             resultados.append(camino_dist_minimo(grafo,salida,llegada))
@@ -31,6 +34,7 @@ def camino_mas_ops(origen,destino,grafo,archivoAero):
     return min(resultados,key=operator.itemgetter(1))
 
 
+# REVIEW: verificar si las ciudades existen
 def camino_mas(modo,origen,destino,grafoPrecios,grafoTiempos,vuelos):
     if (modo != "barato") and (modo != "rapido"):# or origen not in lista_ciudades or destino not in lista_ciudades:
         return False
@@ -89,6 +93,8 @@ def pagerank_aux(grafo,dict):
 
 
 def pagerank(grafo,k):
+    if (k.isdigit() == False) and int(k) <= 0:
+        return False
     factor = 1/len(ver_vertices(grafo))
     parcial = {}
     for vertice in ver_vertices(grafo):
@@ -115,12 +121,55 @@ def pagerank(grafo,k):
 
     resultado.sort(key = operator.itemgetter(1),reverse = True)
 
-    for j in range(k):
+    # REVIEW: cuidado rango de k
+    # TODO:  formato de print
+    for j in range(int(k)):
         print(resultado[j][0], end = " ")
     print()
 
 
-# la copia de aeropuertos puede ser un diccionario key:ciudad, values:aeropuertos y coordenadas
+
+def N_Lugares(grafo, final, inicio, cantidad, lista, n,resultado):
+    if n == cantidad:
+        if lista[len(lista)-1] in ver_adyacentes(grafo,final):
+            pap = lista.copy()
+            resultado.append(pap)
+            return lista
+        return False
+
+    for vertex in ver_adyacentes(grafo,inicio):
+        if len(resultado) != 0:
+            return
+        if vertex in lista:
+            continue
+        lista.append(vertex)
+        if N_Lugares(grafo,final,vertex,cantidad,lista,n+1,resultado) != True:
+            lista.pop()
+            continue
+
+
+
+def vacaciones(origen,k,grafo,aeros):
+    if (k.isdigit() == False) and int(k) <= 0:
+        return False
+    aeroOrigen = obtener_aeropuertos(origen.title(),aeros)
+    resParciales = []
+    resultad = []
+    for item in aeroOrigen:
+        print(item)
+        resParciales.append(item)
+        N_Lugares(grafo,item,item,int(k),resParciales,0,resultad)
+        if len(resultad) != 0:
+            resultad[0].append(item)
+            break
+        resParciales.pop(0)
+
+    # TODO: formato de print
+    # REVIEW: return en caso de que no haya camino
+    print(resultad)
+
+
+
 def menu(archivoAero,archivoVuelos):
     grafoTiempos = crear_grafo()
     grafoPrecios = crear_grafo()
@@ -151,9 +200,37 @@ def menu(archivoAero,archivoVuelos):
             agregar_arista(grafoTiempos,row[0],row[1],int(row[2]))
             agregar_arista(grafoPrecios,row[0],row[1],int(row[3]))
 
-    comando = input("iniico")
-    while(len(comando) > 0):
-        comando = input("en while")
+
+
+    entrada = input()
+    # entrada = ""
+    while(len(entrada) > 0):
+        try:
+            comando,opciones = entrada.split(" ",1)
+        except Exception as e:
+            print("Exception")
+        else:
+            print(comando)
+            opciones = opciones.split(",")
+
+            if len(opciones) == 3 and comando == "camino_mas":
+                print("entra a camino_mas")
+                camino_mas(opciones[0],opciones[1],opciones[2],grafoPrecios,grafoTiempos,copiaAero)
+            elif len(opciones) == 2 and comando == "camino_escalas":
+                print("entra a camino_escalas")
+                camino_escalas(opciones[0],opciones[1],grafoPrecios,copiaAero)
+            elif len(opciones) == 1 and comando == "pagerank":
+                print("entra a pagerank")
+                pagerank(grafoPrecios,opciones[0])
+            elif len(opciones) == 2 and comando == "vacaciones":
+                print("entra a vacaciones")
+                vacaciones(opciones[0],opciones[1],grafoPrecios,copiaAero)
+            else:
+                print("opcion mala")
+        # print(capitalize(opciones[1]))
+        # print(opciones)
+        entrada = input()
+
 
 
 def Main():
